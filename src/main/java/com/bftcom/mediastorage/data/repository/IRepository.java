09045d93cd30e1a845702ca0b8ea128343fa218a
Pavel Korepanov <p.korepanov@bftcom.com>
2023-07-19 09:54:41 +0400

    Исправлен IRepository

--- src/main/java/com/bftcom/mediastorage/data/repository/IRepository.java
+++ src/main/java/com/bftcom/mediastorage/data/repository/IRepository.java
@@ -1,10 +1,11 @@
 package com.bftcom.mediastorage.data.repository;
 
-import java.util.Collection;
+import com.bftcom.mediastorage.data.model.BaseModel;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface IRepository<T, TId> {
+public interface IRepository<T extends BaseModel, TId> {
     Optional<T> findById(TId id);
     List<T> findAll();
     T save(T t);
