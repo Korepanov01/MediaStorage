731aedd8c282c85d8811787290f544cd3a85899c
p.korepanov <p.korepanov@bftcom.com>
2023-07-24 16:03:51 +0400

    Добавлен новый метод в репозиторий тегов

---
+++ src/main/java/com/bftcom/mediastorage/model/parameters/MediaSearchParameters.java
@@ -0,0 +1,26 @@
+package com.bftcom.mediastorage.model.parameters;
+
+import java.util.List;
+
+public class MediaSearchParameters extends SearchStringParameters {
+
+    private List<Long> tagIds;
+
+    private Long categoryId;
+
+    public List<Long> getTagIds() {
+        return tagIds;
+    }
+
+    public void setTagIds(List<Long> tagIds) {
+        this.tagIds = tagIds;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
