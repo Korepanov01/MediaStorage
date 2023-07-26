017a06d84efc953b9d1a01e1f2468a7a0ea18bad
p.korepanov <p.korepanov@bftcom.com>
2023-07-26 10:39:09 +0400

    Выделен базовый абстрактный сервис

--- src/main/java/com/bftcom/mediastorage/service/RoleService.java
+++ src/main/java/com/bftcom/mediastorage/service/RoleService.java
@@ -1,55 +1,29 @@
 package com.bftcom.mediastorage.service;
 
-import com.bftcom.mediastorage.exception.EntityAlreadyExistsException;
-import com.bftcom.mediastorage.exception.EntityNotFoundException;
 import com.bftcom.mediastorage.model.entity.Role;
 import com.bftcom.mediastorage.model.parameters.SearchStringParameters;
+import com.bftcom.mediastorage.repository.ParametersSearchRepository;
 import com.bftcom.mediastorage.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
-public class RoleService implements IService<Role, SearchStringParameters> {
+public class RoleService extends BaseService<Role, SearchStringParameters> {
 
-    private final RoleRepository repository;
+    private final RoleRepository roleRepository;
 
     @Autowired
     public RoleService(RoleRepository repository) {
-        this.repository = repository;
-    }
-
-    public Optional<Role> findById(Long id) {
-        return repository.findById(id);
-    }
-
-    public Optional<Role> findByName(String name) {
-        return repository.findByName(name);
+        this.roleRepository = repository;
     }
 
     @Override
-    public List<Role> findByParameters(SearchStringParameters parameters) {
-        return repository.findByParameters(parameters);
+    protected boolean isEntityExists(Role role) {
+        return roleRepository.findByName(role.getName()).isPresent();
     }
 
     @Override
-    public Role save(Role role) throws EntityAlreadyExistsException {
-        if (repository.findByName(role.getName()).isPresent()) {
-            throw new EntityAlreadyExistsException();
-        }
-        return repository.save(role);
-    }
-
-    @Override
-    public void delete(Long id) throws EntityNotFoundException {
-        Optional<Role> optionalRole = repository.findById(id);
-
-        if(optionalRole.isEmpty()) {
-            throw new EntityNotFoundException();
-        }
-
-        repository.delete(optionalRole.get());
+    protected ParametersSearchRepository<Role, SearchStringParameters> getMainRepository() {
+        return roleRepository;
     }
 }
