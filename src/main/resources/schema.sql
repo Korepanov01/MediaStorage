881130fef4d2b1951b25b3edc8ade36f912b305a
p.korepanov <p.korepanov@bftcom.com>
2023-08-01 11:17:30 +0400

    Улучшены индексы

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,41 +1,40 @@
-drop schema if exists public cascade;
+DROP SCHEMA IF EXISTS public CASCADE;
 
-create schema "public";
+CREATE SCHEMA "public";
 
 
 -- Таблица пользователей
 CREATE TABLE "public.user"
 (
-    "id"                bigserial       CONSTRAINT "user_pk" PRIMARY KEY,
-    "name"              varchar(200)    NOT NULL,
-    "password_hash"     varchar(256)    NOT NULL,
-    "email"             varchar(500)    NOT NULL
+    "id"                BIGSERIAL       CONSTRAINT "user_pk" PRIMARY KEY,
+    "name"              VARCHAR(200)    NOT NULL,
+    "password_hash"     VARCHAR(256)    NOT NULL,
+    "email"             VARCHAR(500)    NOT NULL
 );
 
-CREATE UNIQUE INDEX uidx_user_name     ON "public.user" ("name");
-CREATE UNIQUE INDEX uidx_user_email    ON "public.user" ("email");
+CREATE UNIQUE INDEX uidx_user_name     ON "public.user" (LOWER("name")  varchar_pattern_ops);
+CREATE UNIQUE INDEX uidx_user_email    ON "public.user" (LOWER("email") varchar_pattern_ops);
 
 
 -- Таблица ролей
 CREATE TABLE "public.role"
 (
-    "id"   bigserial    CONSTRAINT "role_pk" PRIMARY KEY,
-    "name" varchar(100) NOT NULL
+    "id"   BIGSERIAL    CONSTRAINT "role_pk" PRIMARY KEY,
+    "name" VARCHAR(100) NOT NULL
 );
 
-CREATE UNIQUE INDEX uidx_role_name ON "public.role" ("name");
+CREATE UNIQUE INDEX uidx_role_name ON "public.role" (LOWER("name") varchar_pattern_ops);
 
 
 -- Таблица связей пользователей и ролей
 CREATE TABLE "public.user_role"
 (
-    "id"        bigserial   CONSTRAINT "user_role_pk" PRIMARY KEY,
+    "id"        BIGSERIAL   CONSTRAINT "user_role_pk" PRIMARY KEY,
     "role_id"   bigint      NOT NULL,
     "user_id"   bigint      NOT NULL
 );
 
-CREATE UNIQUE INDEX uidx_user_role_role_id_role_id ON "public.user_role" ("role_id", "user_id");
-CREATE INDEX idx_user_role_user_id ON "public.user_role" ("user_id");
+CREATE UNIQUE INDEX uidx_user_role_role_id_role_id  ON "public.user_role" ("role_id", "user_id");
 
 ALTER TABLE "public.user_role"
     ADD CONSTRAINT "user_role_fk0"
@@ -52,12 +51,12 @@ ALTER TABLE "public.user_role"
 -- Таблица категорий
 CREATE TABLE "public.category"
 (
-    "id"                 bigserial      CONSTRAINT "category_pk" PRIMARY KEY,
-    "name"               varchar(200)   NOT NULL,
+    "id"                 BIGSERIAL      CONSTRAINT "category_pk" PRIMARY KEY,
+    "name"               VARCHAR(200)   NOT NULL,
     "parent_category_id" bigint
 );
 
-CREATE UNIQUE INDEX uidx_category_name          ON "public.category" ("name");
+CREATE UNIQUE INDEX uidx_category_name          ON "public.category" (LOWER("name") varchar_pattern_ops);
 CREATE INDEX idx_category_parent_category_id    ON "public.category" ("parent_category_id");
 
 ALTER TABLE "public.category"
@@ -70,27 +69,27 @@ ALTER TABLE "public.category"
 -- Таблица типов медиа
 CREATE TABLE "public.media_type"
 (
-    "id"   bigserial    CONSTRAINT "media_type_pk" PRIMARY KEY,
-    "name" varchar(100) NOT NULL
+    "id"   BIGSERIAL    CONSTRAINT "media_type_pk" PRIMARY KEY,
+    "name" VARCHAR(100) NOT NULL
 );
 
-CREATE UNIQUE INDEX uidx_media_type_name ON "public.media_type" ("name");
+CREATE UNIQUE INDEX uidx_media_type_name ON "public.media_type" (LOWER("name") varchar_pattern_ops);
 
 
 -- Таблица медиа
 CREATE TABLE "public.media"
 (
-    "id"            bigserial       CONSTRAINT "media_pk" PRIMARY KEY,
+    "id"            BIGSERIAL       CONSTRAINT "media_pk" PRIMARY KEY,
     "user_id"       bigint          NOT NULL,
     "category_id"   bigint          NOT NULL,
-    "name"          varchar(200)    NOT NULL,
-    "description"   varchar(10000),
+    "name"          VARCHAR(200)    NOT NULL,
+    "description"   VARCHAR(10000),
     "media_type_id" bigint          NOT NULL,
     "created_at"    TIMESTAMP       NOT NULL,
     "edited_at"     TIMESTAMP       NOT NULL
 );
 
-CREATE INDEX idx_media_name             ON "public.media" ("name");
+CREATE INDEX idx_media_name             ON "public.media" (LOWER("name") varchar_pattern_ops);
 CREATE INDEX idx_media_media_type_id    ON "public.media" ("media_type_id");
 CREATE INDEX idx_media_category_id      ON "public.media" ("category_id");
 CREATE INDEX idx_media_created_at       ON "public.media" ("created_at");
@@ -116,24 +115,22 @@ ALTER TABLE "public.media"
 -- Таблица тегов
 CREATE TABLE "public.tag"
 (
-    "id"    bigserial       CONSTRAINT "tag_pk" PRIMARY KEY,
-    "name"  varchar(200)    NOT NULL
+    "id"    BIGSERIAL       CONSTRAINT "tag_pk" PRIMARY KEY,
+    "name"  VARCHAR(200)    NOT NULL
 );
 
-CREATE UNIQUE INDEX uidx_tag_name ON "public.tag"(name);
+CREATE UNIQUE INDEX uidx_tag_name ON "public.tag"(LOWER("name") varchar_pattern_ops);
 
 
 -- Таблица связей медиа и тегов
 CREATE TABLE "public.media_tag"
 (
-    "id"       bigserial    CONSTRAINT "media_tag_pk" PRIMARY KEY,
+    "id"       BIGSERIAL    CONSTRAINT "media_tag_pk" PRIMARY KEY,
     "media_id" bigint       NOT NULL,
     "tag_id"   bigint       NOT NULL
 );
 
 CREATE UNIQUE INDEX uidx_media_tag_media_id_tag_id ON "public.media_tag" ("media_id", "tag_id");
-CREATE INDEX idx_media_tag_media_id ON "public.media_tag" ("media_id");
-CREATE INDEX idx_media_tag_tag_id   ON "public.media_tag" ("tag_id");
 
 ALTER TABLE "public.media_tag"
     ADD CONSTRAINT "media_tag_fk0"
@@ -150,36 +147,36 @@ ALTER TABLE "public.media_tag"
 -- Таблица файлов
 CREATE TABLE "public.file"
 (
-    "id"            bigserial       CONSTRAINT "file_pk" PRIMARY KEY,
-    "name"          varchar(200)    NOT NULL,
-    "content_type"  varchar(50)    NOT NULL,
+    "id"            BIGSERIAL       CONSTRAINT "file_pk" PRIMARY KEY,
+    "name"          VARCHAR(200)    NOT NULL,
+    "content_type"  VARCHAR(50)     NOT NULL,
     "size"          BIGINT          NOT NULL,
-    "data"          bytea           NOT NULL
+    "data"          BYTEA           NOT NULL
 );
 
 
 -- Таблица типов файлов
 CREATE TABLE "public.file_type"
 (
-    "id"   bigserial    CONSTRAINT "file_type_pk" PRIMARY KEY,
-    "name" varchar(100) NOT NULL
+    "id"   BIGSERIAL    CONSTRAINT "file_type_pk" PRIMARY KEY,
+    "name" VARCHAR(100) NOT NULL
 );
 
-CREATE UNIQUE INDEX uidx_file_type_type ON "public.file_type" ("name");
+CREATE UNIQUE INDEX uidx_file_type_type ON "public.file_type" (LOWER("name") varchar_pattern_ops);
 
 
 -- Таблица связей файлов и медиа
 CREATE TABLE "public.media_file"
 (
-    "id"           bigserial    CONSTRAINT "media_file_pk" PRIMARY KEY,
+    "id"           BIGSERIAL    CONSTRAINT "media_file_pk" PRIMARY KEY,
     "media_id"     bigint       NOT NULL,
     "file_id"      bigint       NOT NULL,
     "file_type_id" bigint       NOT NULL
 );
 
-CREATE UNIQUE INDEX uidx_media_file_media_id_file_id ON "public.media_file" ("media_id", "file_id");
-CREATE INDEX idx_media_file_media_id        ON "public.media_file" ("media_id");
-CREATE INDEX idx_media_file_file_type_id    ON "public.media_file" ("file_type_id");
+CREATE UNIQUE INDEX uidx_media_file_media_id_file_id    ON "public.media_file" ("media_id", "file_id");
+CREATE INDEX idx_media_file_media_id                    ON "public.media_file" ("media_id");
+CREATE INDEX idx_media_file_file_type_id                ON "public.media_file" ("file_type_id");
 
 ALTER TABLE "public.media_file"
     ADD CONSTRAINT "media_file_fk0"
