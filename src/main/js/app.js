2c6a9af3cc43a880f46ac14e4110aceb8d671a97
p.korepanov <p.korepanov@bftcom.com>
2023-08-03 09:39:12 +0400

    Добавлен webpack

---
+++ src/main/js/app.js
@@ -0,0 +1,11 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+function App(props) {
+    return <h1>Hi!</h1>;
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
