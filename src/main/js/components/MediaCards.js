f6b0e5c634be3d08e2219a5862f5a499a6b3cdbd
p.korepanov <p.korepanov@bftcom.com>
2023-08-04 15:37:48 +0400

    исправления

--- src/main/js/components/MediaCards.js
+++ src/main/js/components/MediaCards.js
@@ -8,16 +8,16 @@ export function MediaCards({medias: medias, cardsInRow: cardsInRow}) {
     return (
         <CardGroup>
             <Container fluid>
-            {getChunks(medias, cardsInRow).map((chunk, i) => (
-                <Row key={ i } className={"justify-content-between"}>
-                    {chunk.map((media) => (
-                        <Col lg = {12 / cardsInRow} key={media.id}>
-                            <MediaCard  media={media} />
-                        </Col>
-                    ))}
-                </Row>
-            ))}
-                </Container>
+                {getChunks(medias, cardsInRow).map((chunk, i) => (
+                    <Row key={ i } className={"justify-content-between"}>
+                        {chunk.map((media) => (
+                            <Col lg = {12 / cardsInRow} key={media.id}>
+                                <MediaCard  media={media} />
+                            </Col>
+                        ))}
+                    </Row>
+                ))}
+            </Container>
         </CardGroup>
     );
 }
