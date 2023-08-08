69acbe8acf4e0364dd139630ef2b0ffb6b6a51a9
p.korepanov <p.korepanov@bftcom.com>
2023-08-08 17:42:55 +0400

    Исправления

--- src/main/java/com/bftcom/mediastorage/model/api/request/DeleteEntityRequest.java
+++ src/main/java/com/bftcom/mediastorage/model/api/request/DeleteEntityRequest.java
@@ -1,6 +1,4 @@
 package com.bftcom.mediastorage.model.api.request;
 
-import com.bftcom.mediastorage.model.api.request.Request;
-
 public abstract class DeleteEntityRequest extends Request {
 }
--- src/main/java/com/bftcom/mediastorage/model/api/request/PostPutEntityRequest.java
+++ src/main/java/com/bftcom/mediastorage/model/api/request/PostPutEntityRequest.java
@@ -1,8 +1,6 @@
 package com.bftcom.mediastorage.model.api.request;
 
-import com.bftcom.mediastorage.model.api.request.Request;
 import com.bftcom.mediastorage.model.entity.BaseEntity;
-import com.bftcom.mediastorage.model.api.request.ToEntityConvertable;
 
 public abstract class PostPutEntityRequest<Entity extends BaseEntity>
         extends Request
