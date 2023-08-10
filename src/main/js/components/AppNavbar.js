f7de6fe92ab32c53c4003a947ebf9847a12ca5a5
p.korepanov <p.korepanov@bftcom.com>
2023-08-10 12:31:09 +0400

    Окно входа

--- src/main/js/components/AppNavbar.js
+++ src/main/js/components/AppNavbar.js
@@ -5,23 +5,34 @@ const React = require('react');
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {Button, Col, Row} from "react-bootstrap";
+import {useState} from "react";
+import {AuthPopup} from "./popups/AuthPopup";
 
 const PROFILE_TAB_NAME = "Личный кабинет";
 const SEARCH_TAB_NAME = "Поиск";
 
 export function AppNavbar() {
+    const [showLoginForm, setShowLoginForm] = useState(false);
+
     return (
-        <Navbar expand="lg">
-            <Container fluid>
-                <Navbar.Brand as={Link} to="/">MediaStorage</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="search">{SEARCH_TAB_NAME}</Nav.Link>
-                        <Nav.Link as={Link} to="profile">{PROFILE_TAB_NAME}</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Row className={"w-100"}>
+            <Col lg={"10"}>
+                <Navbar expand="lg" className={"w-100"}>
+                    <Navbar.Brand as={Link} to="/">MediaStorage</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="search">{SEARCH_TAB_NAME}</Nav.Link>
+                            <Nav.Link as={Link} to="profile">{PROFILE_TAB_NAME}</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Col>
+            <Col lg={"2"}>
+                <AuthPopup onChangeShow={setShowLoginForm} show={showLoginForm}/>
+                <Button className={"w-100"} onClick={() => setShowLoginForm(true)}>Войти</Button>
+            </Col>
+        </Row>
     );
 }
---
+++ src/main/js/components/popups/AuthPopup.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import {Button, Card, Form, FormGroup, Modal} from "react-bootstrap";
+
+export function AuthPopup({show: show, onChangeShow: handleChangeShow}) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleLoginClick() {
+        handleChangeShow(false);
+    }
+
+    return (
+        <Modal show={show} onHide={() => handleChangeShow(false)}>
+            <Modal.Body>
+                <Form>
+                    <Form.Text>Войти</Form.Text>
+                    <FormGroup>
+                        <Form.Label>Почта</Form.Label>
+                        <Form.Control
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Form.Label>Пароль</Form.Label>
+                        <Form.Control
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                        />
+                    </FormGroup>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={handleLoginClick}>Войти</Button>
+                <Button variant="secondary" onClick={() => handleChangeShow(false)}>
+                    Отмена
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
