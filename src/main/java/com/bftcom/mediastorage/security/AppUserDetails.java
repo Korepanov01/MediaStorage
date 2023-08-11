ec946b6181aeac4e6716b0b6ca7f4c9b7b43e981
p.korepanov <p.korepanov@bftcom.com>
2023-08-11 10:17:55 +0400

    Реализован UserDetails

---
+++ src/main/java/com/bftcom/mediastorage/security/AppUserDetails.java
@@ -0,0 +1,75 @@
+package com.bftcom.mediastorage.security;
+
+import com.bftcom.mediastorage.model.entity.Role;
+import com.bftcom.mediastorage.model.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+public class AppUserDetails implements UserDetails {
+
+    private Long id;
+
+    private String password;
+
+    private String email;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static AppUserDetails build(User user, List<Role> roles) {
+        List<GrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .collect(Collectors.toList());
+
+        return new AppUserDetails(
+                user.getId(),
+                user.getPasswordHash(),
+                user.getEmail(),
+                authorities);
+    }
+}
