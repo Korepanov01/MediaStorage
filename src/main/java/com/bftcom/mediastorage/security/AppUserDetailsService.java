276134042200d1e70581981aa0dd75acc6214bf0
p.korepanov <p.korepanov@bftcom.com>
2023-08-11 10:39:49 +0400

    Написан UserDetailsService

---
+++ src/main/java/com/bftcom/mediastorage/security/AppUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.bftcom.mediastorage.security;
+
+import com.bftcom.mediastorage.model.entity.Role;
+import com.bftcom.mediastorage.model.entity.User;
+import com.bftcom.mediastorage.repository.RoleRepository;
+import com.bftcom.mediastorage.repository.UserRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Пользователь %s не найден", username)));
+        List<Role> roles = roleRepository.findByUserId(user.getId());
+        return AppUserDetails.build(user, roles);
+    }
+}
