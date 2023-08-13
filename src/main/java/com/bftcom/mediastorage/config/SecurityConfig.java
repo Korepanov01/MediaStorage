a058eaf26720defbcdb70f768057db27fcdac850
KorepanovPA <korepanovpa01@gmail.com>
2023-08-13 20:47:56 +0400

    Оптимизированн секурити конфиг

--- src/main/java/com/bftcom/mediastorage/config/SecurityConfig.java
+++ src/main/java/com/bftcom/mediastorage/config/SecurityConfig.java
@@ -62,41 +62,24 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/tags/**").permitAll()
-                .antMatchers("/api/tags/**").hasRole(Role.ADMIN)
+                .antMatchers(HttpMethod.GET, "/api/users/**").hasRole(Role.ADMIN)
 
-                .antMatchers(HttpMethod.GET, "/api/roles/**").permitAll()
-                .antMatchers("/api/roles/**").hasRole(Role.ADMIN)
-
-                .antMatchers(HttpMethod.GET, "/api/media_type/**").permitAll()
-                .antMatchers("/api/media_type/**").hasRole(Role.ADMIN)
-
-                .antMatchers(HttpMethod.GET, "/api/media/**").permitAll()
-                .antMatchers( "/api/media/{id}").access(String.format("(@securityUtils.checkUserId(authentication, #id)) || hasRole(%s)", Role.ADMIN))
-                .antMatchers(HttpMethod.POST, "/api/media/").authenticated()
-
-                .antMatchers(HttpMethod.GET, "/api/file_types/**").permitAll()
-                .antMatchers("/api/file_types/**").hasRole(Role.ADMIN)
-
-                .antMatchers(HttpMethod.GET, "/api/category/**").permitAll()
-                .antMatchers("/api/category/**").hasRole(Role.ADMIN)
+                .antMatchers(HttpMethod.GET).permitAll()
 
                 .antMatchers(HttpMethod.POST, "/api/users/**").permitAll()
                 .antMatchers(HttpMethod.PATCH, "/api/users/**}").authenticated()
-                .antMatchers(HttpMethod.GET, "/api/users/**").hasRole(Role.ADMIN)
                 .antMatchers(HttpMethod.DELETE, "/api/users/{id}").access("(@securityUtils.checkUserId(authentication, #id)) || hasRole(\"ADMIN\")")
 
+                .antMatchers( "/api/media/{id}").access(String.format("(@securityUtils.checkUserId(authentication, #id)) || hasRole(%s)", Role.ADMIN))
+                .antMatchers(HttpMethod.POST, "/api/media/").authenticated()
+
+                .antMatchers("/api/media_tag").authenticated()
+                .antMatchers("/api/files/**").authenticated()
+                .antMatchers("/api/auth/login").permitAll()
 
                 .antMatchers("/api/user_role/**").hasRole(Role.SUPER_ADMIN)
 
-                .antMatchers("/api/media_tag").authenticated()
-
-                .antMatchers(HttpMethod.GET, "/api/files/**").permitAll()
-                .antMatchers("/api/files/**").authenticated()
-
-                .antMatchers("/api/auth/login").permitAll()
-
-                .antMatchers(HttpMethod.GET, "/api/media_file/**").permitAll();
+                .antMatchers("/**").hasRole(Role.ADMIN);
 
         http.authenticationProvider(authenticationProvider);
 
