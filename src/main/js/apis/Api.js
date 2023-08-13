ccb6cb41e3b68d94e92b196d2324b34090232635
KorepanovPA <korepanovpa01@gmail.com>
2023-08-13 23:05:28 +0400

    Исправления

--- src/main/js/apis/Api.js
+++ src/main/js/apis/Api.js
@@ -11,7 +11,7 @@ export const Api = axios.create({
 
 export const useAxios = (url, method, payload) => {
     const [data, setData] = useState(null);
-    const [error, setError] = useState("");
+    const [error, setError] = useState(null);
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
@@ -24,7 +24,22 @@ export const useAxios = (url, method, payload) => {
                 });
                 setData(response.data);
             } catch (error) {
-                setError(error.message);
+                if (error.response) {
+                    setError({
+                        status: error.response.status,
+                        messages: error.response?.data?.errors ? error.response.data.errors : []
+                    });
+                } else if (error.request) {
+                    setError({
+                        status: 0,
+                        messages: ["Нет ответа от сервера"]
+                    });
+                } else {
+                    setError({
+                        status: 0,
+                        messages: ["Неизвестная ошибка"]
+                    });
+                }
             } finally {
                 setLoaded(true);
             }
