ad414a9064513a0ef3b7d6529229e4a864d2b318
p.korepanov <p.korepanov@bftcom.com>
2023-08-14 12:34:04 +0400

    переписана форма входа

--- src/main/js/components/popups/LoginPopup.js
+++ src/main/js/components/popups/LoginPopup.js
@@ -1,42 +1,61 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Button, Form, FormGroup, Modal} from "react-bootstrap";
+import {Formik} from "formik";
+import {object, string} from "yup"
 
 export function LoginPopup({show: show, onChangeShow: handleChangeShow}) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
-    function handleLoginClick() {
+    function handleLoginClick(values) {
+        console.log(JSON.stringify(values))
     }
+
+    const validationSchema = object({
+        email: string().required("Введите почту"),
+        password: string().required("Введите пароль")
+    });
 
     return (
         <Modal show={show} onHide={() => handleChangeShow(false)}>
+            <Modal.Header>
+                <h1 className={"text-center w-100"}>Вход</h1>
+            </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Form.Text>Войти</Form.Text>
-                    <FormGroup>
-                        <Form.Label>Почта</Form.Label>
-                        <Form.Control
-                            type="email"
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Form.Label>Пароль</Form.Label>
-                        <Form.Control
-                            type="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            value={password}
-                        />
-                    </FormGroup>
-                </Form>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => handleLoginClick(values)}
+                    validationSchema={validationSchema}
+                >
+                    {({ handleChange, handleSubmit, values, errors, touched }) => (
+                        <Form onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Form.Label>Почта</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="email"
+                                    onChange={handleChange}
+                                    value={values.email}
+                                    isInvalid={touched.email && errors.email}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                            </FormGroup>
+                            <FormGroup>
+                                <Form.Label>Пароль</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    name="password"
+                                    onChange={handleChange}
+                                    value={values.password}
+                                    isInvalid={touched.password && errors.password}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+                            </FormGroup>
+                            <FormGroup>
+                                <Button type="submit">Войти</Button>
+                            </FormGroup>
+                        </Form>
+                    )}
+                </Formik>
             </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={handleLoginClick}>Войти</Button>
-                <Button variant="secondary" onClick={() => handleChangeShow(false)}>
-                    Отмена
-                </Button>
-            </Modal.Footer>
         </Modal>
     );
 }
