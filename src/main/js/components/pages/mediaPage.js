b1d708182e7f829a4fd11e5e3086bdf00c17201b
p.korepanov <p.korepanov@bftcom.com>
2023-08-15 17:03:22 +0400

    Проверки авторизации и загрузки

--- src/main/js/components/pages/mediaPage.js
+++ src/main/js/components/pages/mediaPage.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import {Col, Row} from "react-bootstrap";
+import {Col, Row, Spinner} from "react-bootstrap";
 import {getMediaById} from "../../apis/mediaAPI";
 import {FilesCarousel} from "../fileCarousel";
 import {MediaInfo} from "../mediaInfo";
@@ -8,19 +8,19 @@ import {FileTypes} from "../../enums/fileTypes";
 import {getMediaFiles} from "../../apis/mediaFileAPI";
 import {MediaRedactor} from "../mediaRedactor";
 import {getTags} from "../../apis/tagAPI";
+import {useSelector} from "react-redux";
 
 export function MediaPage() {
     const {id} = useParams();
 
-    const [media, setMedia] = useState({
-        id: 0,
-        name: "string",
-        user: {
-            id: 0,
-            name: "string"
-        }});
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const authUserId = useSelector(state => state.auth.user.id);
+
     const [mediaFiles, setMediaFiles] = useState([]);
     const [tags, setTags] = useState([])
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [media, setMedia] = useState(null);
 
     useEffect(() => {
         getTags({mediaId: id}).then(result => {
@@ -42,15 +42,22 @@ export function MediaPage() {
 
     return (
         <>
-            <Row>
-                <Col lg={4}>
-                    <MediaInfo media={media} tags={tags}/>
-                    <MediaRedactor media={media} mediaFiles={mediaFiles} tags={tags} onTagsChange={setTags}/>
-                </Col>
-                <Col lg={8}>
-                    <FilesCarousel filesUrls={mediaFiles.filter(mediaFile => mediaFile.fileType === FileTypes.main).map(mediaFile => mediaFile.url)}/>
-                </Col>
-            </Row>
+            {isLoading ? (
+                <Spinner/>
+            ) : (
+                <Row>
+                    <Col lg={4}>
+                        <MediaInfo media={media} tags={tags}/>
+                        {(isLoggedIn && authUserId === media.user.id) &&
+                            <MediaRedactor media={media} mediaFiles={mediaFiles} tags={tags} onTagsChange={setTags}/>
+                        }
+                    </Col>
+                    <Col lg={8}>
+                        <FilesCarousel
+                            filesUrls={mediaFiles.filter(mediaFile => mediaFile.fileType === FileTypes.main).map(mediaFile => mediaFile.url)}/>
+                    </Col>
+                </Row>
+            )}
         </>
     );
 }
