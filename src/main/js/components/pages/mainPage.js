e83fd6bee1ac237e9aa8ac720b26da42adfd14cf
p.korepanov <p.korepanov@bftcom.com>
2023-08-15 15:50:58 +0400

    Исправления

--- src/main/js/components/pages/mainPage.js
+++ src/main/js/components/pages/mainPage.js
@@ -2,14 +2,15 @@ import React, {useEffect, useState} from 'react';
 import {MediaCards} from "../mediaCards";
 import {getMedias} from "../../apis/mediaAPI";
 
+const PARAMS = {pageSize: 20, randomOrder: true};
 const CARDS_IN_ROW = 4;
 
 export function MainPage() {
     const [medias, setMedias] = useState([])
 
     useEffect(() => {
-        getMedias({pageSize: 20, randomOrder: true}).then(({data, error}) => {
-            if (!error) setMedias(data);
+        getMedias(PARAMS).then(({data: medias, error}) => {
+            if (!error) setMedias(medias);
         });
     }, []);
 
