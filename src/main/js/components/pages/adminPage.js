4af21f03590d80c4b05852ba15a135870155151d
p.korepanov <p.korepanov@bftcom.com>
2023-08-18 17:33:33 +0400

    Исправления

--- src/main/js/components/pages/adminPage.js
+++ src/main/js/components/pages/adminPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useLayoutEffect} from 'react';
 import Nav from "react-bootstrap/Nav";
 import {Link, Route, Routes} from "react-router-dom";
 import TagsTable from "../admintables/tagsTable";
@@ -10,27 +10,35 @@ import CategoryRedactor from "../admintables/categoryRedactor";
 
 export function AdminPage() {
     const navigate = useNavigate()
-    const user = useSelector(state => state.auth.user?.roles?.includes(Roles.ADMIN) ?? navigate("/"));
+    const user = useSelector(state => state.auth.user);
+
+    useLayoutEffect(() => {
+        if (!(user?.roles?.includes(Roles.ADMIN) ?? false)) navigate("/");
+    });
 
     return (
         <>
-            <Nav variant="tabs" defaultActiveKey="users">
-                <Nav.Item>
-                    <Nav.Link as={Link} to={"users"} eventKey={"users"}>Пользователи</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to={"tags"} eventKey={"users"}>Теги</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to={"categories"} eventKey={"users"}>Категории</Nav.Link>
-                </Nav.Item>
-            </Nav>
-            <Routes>
-                <Route path='/' element={<UsersTable currentUserId={user.id}/>}/>
-                <Route path='users' element={<UsersTable currentUserId={user.id}/>}/>
-                <Route path="tags" element={<TagsTable/>}/>
-                <Route path="categories" element={<CategoryRedactor/>}/>
-            </Routes>
+            {user &&
+                <>
+                    <Nav variant="tabs" defaultActiveKey="users">
+                        <Nav.Item>
+                            <Nav.Link as={Link} to={"users"} eventKey={"users"}>Пользователи</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} to={"tags"} eventKey={"users"}>Теги</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} to={"categories"} eventKey={"users"}>Категории</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Routes>
+                        <Route path='/' element={<UsersTable currentUserId={user.id}/>}/>
+                        <Route path='users' element={<UsersTable currentUserId={user.id}/>}/>
+                        <Route path="tags" element={<TagsTable/>}/>
+                        <Route path="categories" element={<CategoryRedactor/>}/>
+                    </Routes>
+                </>
+            }
         </>
     );
 }
