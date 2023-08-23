2455e06ae3c2386fea83adfb3e4f0c56f0dc9a4c
p.korepanov <p.korepanov@bftcom.com>
2023-08-23 15:38:09 +0400

    Исправлен контроллер типов медиа

--- src/main/java/com/bftcom/mediastorage/api/controller/MediaTypeController.java
+++ src/main/java/com/bftcom/mediastorage/api/controller/MediaTypeController.java
@@ -4,9 +4,9 @@ import com.bftcom.mediastorage.api.controller.interfaces.GetAllController;
 import com.bftcom.mediastorage.api.controller.interfaces.GetByIdController;
 import com.bftcom.mediastorage.model.dto.MediaTypeDto;
 import com.bftcom.mediastorage.model.entity.MediaType;
-import com.bftcom.mediastorage.model.searchparameters.SearchStringParameters;
+import com.bftcom.mediastorage.service.CrudService;
 import com.bftcom.mediastorage.service.MediaTypeService;
-import com.bftcom.mediastorage.service.ParameterSearchService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,17 +21,17 @@ public class MediaTypeController implements
     private final MediaTypeService mediaTypeService;
 
     @Override
-    public MediaTypeDto convertToListItemDto(MediaType mediaType) {
+    public MediaTypeDto convertToListItemDto(@NonNull MediaType mediaType) {
         return new MediaTypeDto(mediaType);
     }
 
     @Override
-    public MediaTypeDto convertToDto(MediaType mediaType) {
+    public MediaTypeDto convertToDto(@NonNull MediaType mediaType) {
         return new MediaTypeDto(mediaType);
     }
 
     @Override
-    public ParameterSearchService<MediaType, SearchStringParameters> getMainService() {
+    public CrudService<MediaType> getMainService() {
         return mediaTypeService;
     }
 }
