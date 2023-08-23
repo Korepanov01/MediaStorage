0753d4e212f038f5beee6bad253057f924cda0f6
p.korepanov <p.korepanov@bftcom.com>
2023-08-23 14:29:36 +0400

    исправлен сервис файлов

--- src/main/java/com/bftcom/mediastorage/service/FileService.java
+++ src/main/java/com/bftcom/mediastorage/service/FileService.java
@@ -2,17 +2,21 @@ package com.bftcom.mediastorage.service;
 
 import com.bftcom.mediastorage.exception.TooManyFilesException;
 import com.bftcom.mediastorage.model.entity.File;
+import com.bftcom.mediastorage.model.entity.FileType;
 import com.bftcom.mediastorage.model.entity.Media;
 import com.bftcom.mediastorage.repository.CrudRepository;
 import com.bftcom.mediastorage.repository.FileRepository;
+import com.bftcom.mediastorage.repository.FileTypeRepository;
 import com.bftcom.mediastorage.repository.MediaRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,7 @@ public class FileService extends CrudService<File> {
 
     private final FileRepository fileRepository;
     private final MediaRepository mediaRepository;
+    private final FileTypeRepository fileTypeRepository;
 
     static public String getFileUrl(@NonNull Long fileId) {
         return ServletUriComponentsBuilder
@@ -30,8 +35,8 @@ public class FileService extends CrudService<File> {
     }
 
     @Transactional
-    public void save(@NonNull File file, @NonNull Long mediaId, @NonNull Long fileTypeId)
-            throws TooManyFilesException, EntityNotFoundException {
+    public File save(@NonNull MultipartFile multipartFile, @NonNull Long mediaId, @NonNull Long fileTypeId)
+            throws TooManyFilesException, EntityNotFoundException, IOException {
         Media media = mediaRepository.findById(mediaId);
 
         if (media == null)
@@ -40,11 +45,22 @@ public class FileService extends CrudService<File> {
         if (media.getFiles().size() > 5)
             throw new TooManyFilesException("Слишком много файлов");
 
-        fileRepository.save(file);
+        FileType fileType = fileTypeRepository.findById(fileTypeId);
+        if (fileType == null)
+            throw new EntityNotFoundException("Тип файла не найден");
+
+        File file = new File(
+                multipartFile.getOriginalFilename(),
+                multipartFile.getContentType(),
+                multipartFile.getSize(),
+                multipartFile.getBytes(),
+                fileType);;
 
         media.addFile(file);
 
         mediaRepository.update(media);
+
+        return file;
     }
 
     @Override
